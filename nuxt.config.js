118067a9bdee0a3692311eadba0dee2b9fba5be5
Andrey Buchnev <andrey@scada.lv>
2022-02-16 18:16:50 +0200

    add required request header

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,10 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     proxy: true,
-    credentials: false
+    credentials: false,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
   },
 
   proxy: {
